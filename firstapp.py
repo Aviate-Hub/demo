d23a3b9d73334e72b6ec337e0ec4c9c93bf1b544
Nitesh D <niteshd@me.com>
2022-07-13 16:27:35 -0400

    Added additional functionality subtraction, multiplication to firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -4,4 +4,12 @@ y = 2
 def calc_sum(x, y):
 	return x + y
 
-print("Hello World! ", calc_sum(x, y))
+def calc_mul(x, y):
+	return x * y
+
+def calc_sub(x, y):
+	return x - y
+
+print("Sum: ", calc_sum(x, y))
+print("Multiplication: ", calc_mul(x, y))
+print("Subtraction: ", calc_sub(x, y))
