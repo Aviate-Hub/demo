a04ccc8ef5b4b499e4de5c87cde3102f79a1edd1
Nitesh D <niteshd@me.com>
2022-07-14 11:12:36 -0400

    Added division to firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -10,6 +10,10 @@ def calc_mul(x, y):
 def calc_sub(x, y):
 	return x - y
 
+def calc_div(x, y):
+	return x / y
+
 print("Sum: ", calc_sum(x, y))
 print("Multiplication: ", calc_mul(x, y))
 print("Subtraction: ", calc_sub(x, y))
+print("Divide: ", calc_div(x, y))
